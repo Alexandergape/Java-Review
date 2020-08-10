5161805f1969803401d4d1444088bb4b0bb884a7
Alexander Peñaloza <alex.997.gape@hotmail.com>
2020-08-09 22:43:22 -0500

    22:42 August 9, 2020
    ahh

--- src/DataStructureAndAlgorithms/Main.java
+++ src/DataStructureAndAlgorithms/Main.java
@@ -1,12 +1,75 @@
 package DataStructureAndAlgorithms;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]){
-        SinglyLinkedList<Integer> mySL = new SinglyLinkedList<Integer>();
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> mySL = new SinglyLinkedList<>();
 
         mySL.addFirst(78);
-        System.out.println(mySL.first());
-//        mySL.
+        mySL.addLast(50);
+        mySL.addLast(940);
+
         System.out.print("AHHHHHH");
+        int a = 8;
+        System.out.println(mySL.getClass().getName());
+        System.out.println(a);
+
+        for (Integer i : mySL)
+            System.out.println(i);
+
+        System.out.println("\nNew method");
+        int cont = 0;
+//        while(mySL.getI(cont)!=null)
+        while (cont < mySL.size())
+            System.out.println(mySL.getI(cont++));
+
+        System.out.println("\nNew method 2");
+//        while (mySL.iterator().hasNext()) { // not working idk why
+//            Integer val = mySL.iterator().next();
+//            System.out.println(val);
+//        }
+        func();
+    }
+
+    public static void func(){
+        SinglyLinkedList<String> myList = new SinglyLinkedList<>();
+        int opt;
+        do {
+            System.out.println("1. Create");
+            System.out.println("2. Read");
+            System.out.println("3. Update");
+            System.out.println("4. Delete");
+            System.out.println("0. EXIT");
+            Scanner S = new Scanner(System.in);
+            System.out.println("Your selection: ");
+            opt = S.nextInt();
+            if(opt==0)break;
+
+            System.out.println();
+            switch (opt) {
+                case 1:
+                    System.out.println("Enter the position & the object");
+                    int pos=S.nextInt();
+                    S.nextLine();
+                    String str= S.nextLine();
+                    myList.add(pos, str);
+                    break;
+                case 2:
+                    System.out.println("All the elements in the list are: ");
+                    for (String s : myList)
+                        System.out.println(s);
+                    break;
+                case 3:
+                    System.out.println("Enter the position & the new object");
+                    myList.update(S.nextInt(), S.nextLine());
+                    break;
+                case 4:
+                    System.out.println("Enter the position of the object to delete");
+                    myList.delete(S.nextInt());
+                    break;
+            }
+        }while (opt!=0);
+
     }
 }
--- src/DataStructureAndAlgorithms/SinglyLinkedList.java
+++ src/DataStructureAndAlgorithms/SinglyLinkedList.java
@@ -1,46 +1,81 @@
 package DataStructureAndAlgorithms;
 
-public class SinglyLinkedList<E> {
+import java.util.Iterator;
 
-    private static class Node<E>{
+public class SinglyLinkedList<E> implements Iterable<E> {
+
+    private static class Node<E> {
         private E element;
         private Node<E> next;
 
-        public Node(E e, Node<E> n){
+        public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
-        
-        public E getElement() {return element;}
-        public Node<E> getNext(){return next;}
-        public void setNext(Node<E> n){next = n;}
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
     }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return (currentIndex < size/* && (getI(currentIndex)!=null)*/);
+            }
+
+            @Override
+            public E next() {
+                return getI(currentIndex++);
+            }
+        };
+    }
+
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
-    public SinglyLinkedList(){}
 
-    public int size(){return size;}
-    public boolean isEmpty(){return size == 0;}
-    public E first(){
-        if (isEmpty())return null;
+    public int size() {
+        return size;
+//        System.out.println(SinglyLinkedList<>[0]);
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() {
+        if (isEmpty()) return null;
         return head.getElement();
     }
-    public E last(){
-        if(isEmpty())return null;
+
+    public E last() {
+        if (isEmpty()) return null;
         return tail.getElement();
     }
-    public void addFirst(E e){
+
+    public void addFirst(E e) {
         head = new Node<>(e, head);
-        if(size == 0)
+        if (size == 0)
             tail = head;
         size++;
     }
 
     public void addLast(E e) {
         Node<E> newest = new Node<>(e, null);
-        if (isEmpty())
-            head = newest;
+        if (isEmpty()) head = newest;
         else
             tail.setNext(newest);
         tail = newest;
@@ -57,12 +92,145 @@ public class SinglyLinkedList<E> {
         return answer;
     }
 
-    public void getAll() {
-        if (isEmpty()) System.out.println("\nThe list is empty.\n");
-        ;
-        E e = head.getElement();
-//        Node node;
-//        head=
+    // The next methods were added by me
+
+    public E getI(int pos) {
+//        if (!(pos>=0 && pos <size))
+//            System.out.println("\nThe entered position is not valid.\n");
+//        else {
+        Node<E> headCopy = head;
+        while (pos > 0) {
+//                System.out.print(headCopy.element.toString() + '\t');
+            headCopy = headCopy.getNext();
+            pos--;
+        }
+//            if (headCopy.getElement()!=null) {
+//                System.out.println("\nnull0");
+////                return null;
+//            }else System.out.println("nu11");
+        return headCopy.getElement();
+//        }
     }
 
+    private Node<E> getNode(int pos){
+        Node<E> headCopy = head;
+        while (pos > 0) {
+            headCopy = headCopy.getNext();
+            pos--;
+        }
+        return  headCopy;
+    }
+
+//    public E getAll(){ // already developed above
+//        Node<E> headCopy = head;
+//        while (headCopy!=null) {
+////                System.out.print(headCopy.element.toString() + '\t');
+//            headCopy = headCopy.getNext();
+//        }
+//        yield headCopy.getElement();
+//    }
+
+//    public SinglyLinkedList<E> report() { // list all the elements in the list
+//        if (isEmpty()) System.out.println("\nThe list is empty.\n");
+////        else {
+////            Node<E> headCopy = head;
+////            while (headCopy != null) {
+////                System.out.print(headCopy.element.toString() + '\t');
+////                headCopy = headCopy.getNext();
+////            }
+////        }
+//        return this;
+//    }
+
+    public int search(E e) { // prints out all the matches of the searched object
+        if (isEmpty()) return -1;
+        Node<E> headCopy = head;
+        int pos = 0;
+        while (headCopy != null) {
+            if (head.toString().equals(e.toString()))
+                break;
+            pos++;
+            headCopy = headCopy.getNext();
+        }
+        return pos; //if success, return the position where the object was found
+    }
+
+    public int update(int pos, E newVal) {
+        if (pos < 0 || pos >= size)
+            return 1; //error
+//            System.out.println("\nThe entered position is not valid.\n");
+//        Node<E> headCopy = head;
+//        for (int i = 0; i < this.size; i++)
+//            headCopy = headCopy.next;
+        getNode(pos).element=newVal;
+//        headCopy.element = newVal;
+//            for (E e : this)
+        return 0;
+    }
+
+    public int delete(int pos) {
+        if (pos < 0 || pos >= size)
+            return 1; //error
+
+//        this.getI(pos);
+        if (pos==0) // first position
+                head=head.getNext();
+        else if(pos<size-1){ // intermediate position
+            Node<E> aux = getNode(pos-1);
+            aux.setNext(getNode(pos+1));
+        }else{ // last position
+            getNode(size-1).setNext(null);
+//            this.tail=null;
+        }
+        size--;
+        if (size==0)
+            tail=null;
+        return 0;
+    }
+
+    public int add(int pos, E e){
+        if (pos < 0 || pos > size)
+            return 1; //error
+        if (pos==0) {
+//            Node<E> aux=new Node<>(e, head);
+//            if (size==0)
+//                tail.setNext(new Node<>(e, null));
+
+            head = new Node<>(e, head);
+        }
+        else if(pos<size-1){
+//            getNode(pos).setNext(new Node<>(e, getNode(pos)/*null*/));
+//            getNode(pos)= new Node<>(e, getNode(pos)/*null*/);
+//
+            Node<E> aux=head;
+            for (int i=0;i<pos;i++){
+                head=head.getNext();
+            }
+            head=new Node<>(e, getNode(pos)/*null*/);
+            head=aux;
+//            getNode(pos).setNext(getNode(pos+1));
+        }else{
+            Node<E> aux=new Node<>(e,null);
+            tail.setNext(aux);
+            tail=aux;
+        }
+        if(size==0)tail=head;
+        size++;
+        return 0; // success
+    }
 }
+//    public void addFirst(E e) {
+//        head = new Node<>(e, head);
+//        if (size == 0)
+//            tail = head;
+//        size++;
+//    }
+//
+//    public void addLast(E e) {
+//        Node<E> newest = new Node<>(e, null);
+//        if (isEmpty()) head = newest;
+//        else
+//            tail.setNext(newest);
+//        tail = newest;
+//        size++;
+//    }
